19f7b52b00506b20e60973a73f0ecb6827e17ce8
dexcarva <69867572+dexcarva@users.noreply.github.com>
2025-11-30 03:10:12 -0500

    refactor: Simplify application entry point

--- albion_insight/__main__.py
+++ albion_insight/__main__.py
@@ -1,6 +1,6 @@
 """Módulo principal para execução da aplicação Albion Insight."""
 
-from albion_insight.app import main
+from albion_insight.core.main_logic import main
 
 if __name__ == "__main__":
     main()
